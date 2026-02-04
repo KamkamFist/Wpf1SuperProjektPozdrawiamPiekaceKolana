ed8d18767d106d5b1232dfc3dcfe0d8a381b6d05
4313657 <4313657@TE-GDA-D-D21E01>
2026-02-04 12:13:50 +0100

    Aktualizacja obsługi suwaków i etykiet RGB
    Zmieniono wartości początkowe etykiet liczników RGB na 255 oraz ujednolicono szerokość suwaka GreenSlider. Dodano mechanizm blokujący obsługę zdarzeń ValueChanged podczas inicjalizacji. Dodano metodę UpdateColor2() do aktualizacji tła i tekstu etykiety rgbLabel. Przycisk teraz wywołuje UpdateColor2() zamiast UpdateColor().

--- WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/MainWindow.xaml.cs
@@ -16,24 +16,32 @@ namespace WpfApp1
     /// </summary>
     public partial class MainWindow : Window
     {
+        bool sex = false;
         public MainWindow()
         {
             InitializeComponent();
+            sex= true;
         }
       
         private void RedSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
         {
+            if (sex == false) return;
             RedCount.Content = Math.Round(RedSlider.Value);
+            UpdateColor();
         }
 
         private void GreenSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
         {
+            if (sex == false) return;
             GreenCount.Content = Math.Round(GreenSlider.Value);
+            UpdateColor();
         }
 
         private void BlueSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
         {
+            if (sex == false) return;
             BlueCount.Content = Math.Round(BlueSlider.Value);
+            UpdateColor();
         }
         private void UpdateColor()
         {
@@ -41,12 +49,18 @@ namespace WpfApp1
             byte g = (byte)Math.Round(GreenSlider.Value);
             byte b = (byte)Math.Round(BlueSlider.Value);
             ColorRectangle.Fill = new SolidColorBrush(Color.FromRgb(r, g, b));
+        }
+        private void UpdateColor2()
+        {
+            byte r = (byte)Math.Round(RedSlider.Value);
+            byte g = (byte)Math.Round(GreenSlider.Value);
+            byte b = (byte)Math.Round(BlueSlider.Value);
+            rgbLabel.Background = new SolidColorBrush(Color.FromRgb(r, g, b));
             rgbLabel.Content = $"{r}, {g}, {b}";
         }
-
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            UpdateColor();
+            UpdateColor2();
         }
     }
 }
